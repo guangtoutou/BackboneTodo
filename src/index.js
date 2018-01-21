26bc28c36246ec174092243b21e612f39d3e2bd9
guangtoutou <ni.ningning@gmail.com>
2018-01-21 20:34:05 +0800

    use webpack css loader and create a css file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import $ from 'jquery'
+import $ from 'jquery';
+import _ from 'underscore';
+import Backbone from 'backbone';
+import './css/style.css'
+
+var Todo = Backbone.Model.extend({
+	defaults:{
+		title:'',
+		completed: false
+	}
+});
+
+
+var TodoView = Backbone.View.extend({
+	el: "#app",
+
+	initialize:function(){
+		this.render();
+	},
+
+	template: _.template($('#item-template').html()),
+
+	render:function(){
+		console.log(this.model.attributes);
+		this.$el.html(this.template(this.model.attributes));
+		return this;
+	}
+});
 
 $(function(){
-	$("#app").append('<h1>hello world</h1>');
+	var todoModel = new Todo({title:'james', completed:false})
+	var todoView = new TodoView({model:new Todo()});
 })
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
 	devtool: 'eval-source-map',
 	output: {
 		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+		{
+			test: /\.css$/,
+			use: [
+			'style-loader',
+			'css-loader'
+			]
+		}
+		]
 	}
 };
